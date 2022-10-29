9d4700562c278017025cd4a859e46973838693fc
Eudes Serpa <48723112+EudesSerpa@users.noreply.github.com>
2022-10-29 16:32:42 -0500

    Update endpoint for the services

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,1 +1,5 @@
-export const BASE_URL = "http://localhost:3000/api/v1";
+// Local environment 
+// export const BASE_URL = "http://localhost:3000/api/v1";
+
+// Production environment 
+export const BASE_URL = "sports-markers.up.railway.app";
